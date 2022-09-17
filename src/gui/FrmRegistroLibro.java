03de918bded79b89e1db7d9cc395fbfd295d2afe
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2022-09-17 13:09:34 -0500

    Ajustes

--- src/gui/FrmRegistroLibro.java
+++ src/gui/FrmRegistroLibro.java
@@ -1,25 +1,18 @@
 package gui;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JButton;
 import javax.swing.JTextField;
-
-import util.Conversiones;
-import util.Validaciones;
-import entidad.Registro_Libro;
-import model.LibroModel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.JTextPane;
 
 public class FrmRegistroLibro extends JInternalFrame implements ActionListener, KeyListener {
 
@@ -126,7 +119,7 @@ public class FrmRegistroLibro extends JInternalFrame implements ActionListener, 
 		}
 	}
 	protected void actionPerformedBtnNewButtonJButton(ActionEvent e) {
-		String ti=texttitulo.getText();
+		/*String ti=texttitulo.getText();
 		String annio=textanhio.getText();
 		String ser=textSerie.getText();
 		String cate=textCategoria.getText();
@@ -155,7 +148,7 @@ public class FrmRegistroLibro extends JInternalFrame implements ActionListener, 
 				mensaje("Error en el registro");
 			}
 			
-		}
+		}*/
 	}
 		public void mensaje(String ms){
 			JOptionPane.showMessageDialog(this, ms);
