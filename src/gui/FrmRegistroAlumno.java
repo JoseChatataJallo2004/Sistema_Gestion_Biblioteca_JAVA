da9152f5e624a229c06513705d9464bf1c734742
VerselEXE <I202123660@cibertec.edu.pe>
2022-09-17 12:40:19 -0500

    Cambio en Registro Alumno

--- src/gui/FrmRegistroAlumno.java
+++ src/gui/FrmRegistroAlumno.java
@@ -2,6 +2,8 @@ package gui;
 
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class FrmRegistroAlumno extends JInternalFrame {
 
@@ -15,5 +17,10 @@ public class FrmRegistroAlumno extends JInternalFrame {
 		setTitle("Ingreso de Alumno");
 		setBounds(100, 100, 900, 600);
 		getContentPane().setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("Registro de Alumno");
+		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblTitulo.setBounds(491, 36, 192, 35);
+		getContentPane().add(lblTitulo);
 	}
 }
